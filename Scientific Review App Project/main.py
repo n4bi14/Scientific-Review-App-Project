89e4e96f234394bfa420f4e68f57445e43f21486
n4bi14 <131011110+n4bi14@users.noreply.github.com>
2024-04-19 11:17:07 -0400

    Updated Main.py.

--- Scientific Review App Project/main.py
+++ Scientific Review App Project/main.py
@@ -1,4 +1,3 @@
-from AccessPubmedOA import AccessPubmedOA
 from AccessPubmedPM import AccessPubmedPM
 import GetFullText as oa
 import SummaryGeneration as gemini
@@ -6,13 +5,13 @@ import SummaryGeneration as gemini
 def main():
     chosen_articles = []
     count = 0
-
+    print()
     user_query = input("Please enter in your keywords. If it includes a phrase, put quotation marks around the phrase: ")
     pm = AccessPubmedPM(user_query)
     
     chosen_articles = pm.findArticles()
     
-    #summaryGenerator = gemini.SummaryGeneration()
-    #summaryGenerator.generate_summaries(open_texts)
+    summaryGenerator = gemini.SummaryGeneration()
+    summaryGenerator.generate_summaries(chosen_articles)
     
 main()
