0bf679eb6898d3deca49a0cae863c843f4c04b08
Jacob Galyean <jacob.t.galyean@gmail.com>
2024-04-18 23:06:49 -0400

    Made it where the relevance system of Pubmed is used.

--- Scientific Review App Project/AccessPubmedPM.py
+++ Scientific Review App Project/AccessPubmedPM.py
@@ -1,5 +1,6 @@
 from Bio import Entrez
 import GetFullText as full_text
+import csv
 
 #Program Description:
 #Allows access of Pubmed articles via E-Utilities (the common way of accessing Pubmed).
@@ -13,17 +14,23 @@ class AccessPubmedPM:
     def __init__(self, query):
          self.query = query
          self.valid_articles = []
+         self.csv_file_path = 'PMC-ids.csv'
 
     def findArticles(self):
         count = 0
-        article_ids = ""
+        article_ids = []
         
-        handle = Entrez.esearch(db = "pmc", retmax = 3000, term = self.query)
+        handle = Entrez.esearch(db = "pubmed", retmax = 3000, term = self.query)
         records = Entrez.read(handle)
         handle.close()
+        pmidMap = self.create_pmid_to_pmcid_mapping()
         
-        data = self.getArticleInfo(records['IdList'])
-
+        for record in records["IdList"]:
+            if(record in pmidMap):
+                article_ids.append(pmidMap.get(record)) 
+        
+        data = self.getArticleInfo(article_ids)
+        
         for record in data:
             
             if('body' in record):
@@ -34,11 +41,26 @@ class AccessPubmedPM:
                 
                     if(count >= 100):
                        break
-
+                   
         return self.valid_articles 
             
     def getArticleInfo(self,articles):
         fetchHandle=Entrez.efetch(db="pmc",id=articles,rettype="full",retmode="xml")
-        xml_data = Entrez.read(fetchHandle)
-            
+        xml_data = Entrez.read(fetchHandle, validate=False)
+        
         return xml_data
+
+    
+    # Function to create PMC ID to PubMed ID mapping from a CSV file
+    def create_pmid_to_pmcid_mapping(self):
+        pmid_to_pmcid_mapping = {}  # Initialize an empty dictionary
+
+        with open(self.csv_file_path, mode='r', newline='') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=',')  # Using comma as the delimiter
+            for row in reader:
+                pmcid = row['PMCID']  # Get PMC ID from the 'PMCID' column
+                pmid = row['PMID']    # Get PubMed ID from the 'PMID' column
+                if pmcid and pmid:    # Check if both PMC ID and PubMed ID are present
+                    pmid_to_pmcid_mapping[pmid] = pmcid  # Add mapping to the dictionary
+
+        return pmid_to_pmcid_mapping
