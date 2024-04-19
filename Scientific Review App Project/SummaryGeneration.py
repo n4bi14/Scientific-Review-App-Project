c44196cff6c757a3bdb25f0478f335245e03fdb5
n4bi14 <131011110+n4bi14@users.noreply.github.com>
2024-04-19 11:15:39 -0400

    Updated to include the title of an article.

--- Scientific Review App Project/SummaryGeneration.py
+++ Scientific Review App Project/SummaryGeneration.py
@@ -1,6 +1,12 @@
 import os
 import google.generativeai as genai 
-from AccessPubmedOA import AccessPubmedOA
+from AccessPubmedPM import AccessPubmedPM
+
+#Program Description:
+#This program utilizes the Gemini AI model to generate summaries of academic articles retrieved from PubMed. 
+#It takes relevant articles based on the user's query and returns a summary of each article. 
+#An API key is required for access to the generative model.
+
 
 # Set up API key and model
 os.environ["API_KEY"] = ""
@@ -8,19 +14,18 @@ genai.configure(api_key=os.environ["API_KEY"])
 model = genai.GenerativeModel('gemini-pro')
 
 class SummaryGeneration:
-    
-    def generate_summaries(self, article_fulltexts):
-        #for index, (title, abstract, article_texts) in enumerate(zip(self.titles, self.abstracts, self.full_text), start=1):
-        
-        for article in article_fulltexts:
-            #responseAbs = model.generate_content(f"Please generate a 1-2 sentence max summary of the abstract for the article: {abstract}")
-            responseFullText = model.generate_content(f"Please generate a one to two paragraph max summary of the paper': {article}")
-
-            #summaryAbs = responseAbs.text
+    @staticmethod
+    def generate_summaries(articles):
+        for index, article in enumerate(articles, start=1):
+            # Split the article into title and body
+            article_parts = article.split('Body:')
+            article_title = article_parts[0].replace('Title: ', '')  # Extract title
+            article = article_parts[1]  # Extract body
+            
+            responseFullText = model.generate_content(f"Please generate a one to two paragraph summary of the paper. Paragraphs only. Do not include any special formatting.': {article}")
             summaryFullText = responseFullText.text
-
-            #print(f"Article {index}:")
-            #print(f"Title: {title}")
-            #print(f"Abstract Summary: {summaryAbs}")
-            print(f"Paper Summary: {summaryFullText}")
-            print()
+            
+            print(f"{index}. {article_title} ")
+            print(f"Summary: {summaryFullText}")
+            print() 
+            
